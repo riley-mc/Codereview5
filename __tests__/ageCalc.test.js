16653c380103a9e4b7df0e2c95188851b6d39678
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-13 14:32:53 -0800

    Failed Mars test.

--- __tests__/ageCalc.test.js
+++ __tests__/ageCalc.test.js
@@ -21,7 +21,7 @@ describe('Calculator', () => {
   });
 
   test("will return the users age on Mars", () => {
-    expect(calc.marsAge()).toEqual(10.64);
+    expect(calc.marsAge()).toEqual(10.67);
   });
 
   
