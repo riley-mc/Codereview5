2f289c7bb4d5929c42cd4ea85e5ecae311830523
Riley McAdoo <riley.mcadoo@gmail.com>
2020-11-22 13:01:01 -0800

    Continue to add a template literal.

--- src/main.js
+++ src/main.js
@@ -10,11 +10,27 @@ function printToPage(calc) {
   age = calc.mercuryAge();
   $("#results").append(`<li>Your age on Mercury is ${age} years old.</li>`);
   age = calc.venusAge();
-  $("#results").append(`<li>Your age on Mercury is ${age} years old.</li>`);
+  $("#results").append(`<li>Your age on Venus is ${age} years old.</li>`);
   age = calc.marsAge();
-  $("#results").append(`<li>Your age on Mercury is ${age} years old.</li>`);
+  $("#results").append(`<li>Your age on Mars is ${age} years old.</li>`);
   age = calc.jupiterAge();
-  $("#results").append(`<li>Your age on Mercury is ${age} years old.</li>`);
+  $("#results").append(`<li>Your age on Jupiter is ${age} years old.</li>`);
   age = calc.remainingYears();
+
+  if (age > 0) {
+    $("#results").append(`<li>The average life expectancy is 80 years old, meaning you have about ${age} years left.</li>`);
+    age = calc.remainingMercury();
+    $("#results").append(`<li>You have ${age} years left to live on Mercury.</li>`);
+    age = calc.remainingVenus();
+    $("#results").append(`<li>You have ${age} years left to live on Venus.</li>`);
+    age = calc.remainingMars();
+    $("#results").append(`<li>You have ${age} years left to live on Mars.</li>`);
+    age = calc.remainingJupiter();
+    $("#results").append(`<li>You have ${age} years left to live on Jupiter.</li>`);
+  } else {
+    age = calc.pastExpect();
+    $("results").append(`<li>You have lived past your life expectancy. YOu have lived ${age} years!`);
+  }
 }
+
 $;
